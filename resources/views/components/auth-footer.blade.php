91233ef522e81fd5e3f900ad14e57efa3ca1b8ef
Adri Khamid <muhamad.adri.ft21@mail.umy.ac.id>
2024-10-22 11:29:01 +0700

    install fortify and stisla

---
+++ resources/views/components/auth-footer.blade.php
@@ -0,0 +1,3 @@
+  <div class="simple-footer">
+      Copyright &copy; Lembaga Sistem Informasi UMY
+  </div>
---
+++ resources/views/components/auth-header.blade.php
@@ -0,0 +1,3 @@
+<div class="login-brand">
+    <img src="{{ asset('img/trace.svg') }}" alt="logo" width="300" class="square">
+</div>
