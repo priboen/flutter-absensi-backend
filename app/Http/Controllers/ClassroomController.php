e6bda0882c964c7ff0cc569991d411c04ebdc636
Adri Khamid <muhamad.adri.ft21@mail.umy.ac.id>
2024-10-23 10:25:16 +0700

    make an controller for Classrooms

---
+++ app/Http/Controllers/ClassroomController.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Classroom;
+use Illuminate\Http\Request;
+
+class ClassroomController extends Controller
+{
+    public function index()
+    {
+        $classroom = Classroom::where('name', 'like', '%' . request('name') . '%')
+            ->orderBy('id', 'desc')
+            ->paginate(10);
+        return view('pages.classrooms.index', compact('classroom'));
+    }
+    public function create()
+    {
+        return view('pages.classrooms.create');
+    }
+    public function store(Request $request)
+    {
+        $request->validate([
+            'name' => 'required',
+        ]);
+        Classroom::create([
+            'name' => $request->name,
+            'building_name' => $request->building_name,
+            'latitude' => $request->latitude,
+            'longitude' => $request->longitude,
+        ]);
+        return redirect()->route('classrooms.index')->with('success', 'Classroom created successfully');
+    }
+    public function edit(Classroom $classroom)
+    {
+        return view('pages.classrooms.edit', compact('classroom'));
+    }
+    public function update(Request $request, Classroom $classroom)
+    {
+        $request->validate([
+            'name' => 'required',
+        ]);
+        $classroom->update([
+            'name' => $request->name,
+            'building_name' => $request->building_name,
+            'latitude' => $request->latitude,
+            'longitude' => $request->longitude,
+        ]);
+        return redirect()->route('classrooms.index')->with('success', 'Classroom updated successfully');
+    }
+    public function destroy(Classroom $classroom)
+    {
+        $classroom->delete();
+        return redirect()->route('classrooms.index')->with('success', 'Classroom deleted successfully');
+    }
+}
