d6853f2f8a283019ffc897bccb07f7e7b2d13708
Adri Khamid <muhamad.adri.ft21@mail.umy.ac.id>
2024-10-26 08:41:30 +0700

    declare api route

--- routes/api.php
+++ routes/api.php
@@ -1,8 +1,20 @@
 <?php
 
+use App\Http\Controllers\api\AttendanceController;
+use App\Http\Controllers\api\ClassesController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
+
+//login
+Route::post('/login', [App\Http\Controllers\Api\AuthController::class, 'login']);
+
+//logout
+Route::post('/logout', [App\Http\Controllers\Api\AuthController::class, 'logout'])->middleware('auth:sanctum');
+
+Route::get('/courses', [ClassesController::class, 'getCoursesByStudent'])->middleware('auth:sanctum');
+
+Route::post('/courses/checkin', [AttendanceController::class, 'checkin'])->middleware('auth:sanctum');
