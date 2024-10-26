6ee9bdb9cdadfe1fee1bfebeb8de0e4f465c7e2c
Adri Khamid <muhamad.adri.ft21@mail.umy.ac.id>
2024-10-26 09:49:00 +0700

    update profile

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\api\AuthController;
 use App\Http\Controllers\api\AttendanceController;
 use App\Http\Controllers\api\ClassesController;
 use Illuminate\Http\Request;
@@ -9,11 +10,9 @@ Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
 
-//login
-Route::post('/login', [App\Http\Controllers\Api\AuthController::class, 'login']);
-
-//logout
-Route::post('/logout', [App\Http\Controllers\Api\AuthController::class, 'logout'])->middleware('auth:sanctum');
+Route::post('/login', [AuthController::class, 'login']);
+Route::post('/logout', [AuthController::class, 'logout'])->middleware('auth:sanctum');
+Route::post('/update-profile', [AuthController::class, 'updateProfile'])->middleware('auth:sanctum');
 
 Route::get('/courses', [ClassesController::class, 'getCoursesByStudent'])->middleware('auth:sanctum');
 
