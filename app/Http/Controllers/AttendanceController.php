794aec86b094c02702ad6c7459171e3a01bc5eba
Adri Khamid <muhamad.adri.ft21@mail.umy.ac.id>
2024-10-26 08:41:56 +0700

    change format time type

--- app/Http/Controllers/AttendanceController.php
+++ app/Http/Controllers/AttendanceController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Models\Attendance;
+use Carbon\Carbon;
 use Illuminate\Http\Request;
 
 class AttendanceController extends Controller
@@ -10,20 +11,6 @@ class AttendanceController extends Controller
     public function index(Request $request)
     {
         $query = Attendance::with(['class', 'classroom']);
-
-        // $query->when($request->has('name'), function ($q) use ($request) {
-        //     $q->whereHas('user', function ($q) use ($request) {
-        //         $q->where('name', 'like', '%' . $request->name . '%')
-        //             ->orWhere('unique_number', 'like', '%' . $request->name . '%');
-        //     });
-        // });
-
-        // $query->when($request->has('course'), function ($q) use ($request) {
-        //     $q->whereHas('class.course', function ($q) use ($request) {
-        //         $q->where('name', 'like', '%' . $request->course . '%');
-        //     });
-        // });
-
         $query->when($request->has('classroom'), function ($q) use ($request) {
             $q->whereHas('classroom', function ($q) use ($request) {
                 $q->where('name', 'like', '%' . $request->course . '%');
@@ -37,6 +24,11 @@ class AttendanceController extends Controller
         });
 
         $attendances = $query->orderBy('id', 'desc')->paginate(10);
+        $attendances->getCollection()->transform(function ($attendance) {
+            $attendance->time_in = $attendance->time_in ? Carbon::parse($attendance->time_in)->format('H:i') : null;
+            $attendance->time_out = $attendance->time_out ? Carbon::parse($attendance->time_out)->format('H:i') : null;
+            return $attendance;
+        });
         return view('pages.attendances.index', compact('attendances'));
     }
 }
