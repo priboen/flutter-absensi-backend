e8212f33a853d3cdea1077e4fa31b92a17503dc9
Adri Khamid <muhamad.adri.ft21@mail.umy.ac.id>
2024-11-12 00:00:49 +0700

    create profile page

---
+++ resources/views/pages/users/show.blade.php
@@ -0,0 +1,65 @@
+@extends('layouts.app')
+
+@section('title', 'Profile')
+
+@push('style')
+    <!-- CSS Libraries -->
+    <link rel="stylesheet" href="{{ asset('library/summernote/dist/summernote-bs4.css') }}">
+    <link rel="stylesheet" href="{{ asset('library/bootstrap-social/assets/css/bootstrap.css') }}">
+@endpush
+
+@section('main')
+    <div class="main-content">
+        <section class="section">
+            <div class="section-header">
+                <h1>Profile</h1>
+                <div class="section-header-breadcrumb">
+                    <div class="breadcrumb-item active"><a href="#">Dashboard</a></div>
+                    <div class="breadcrumb-item">Profile</div>
+                </div>
+            </div>
+            <div class="section-body">
+                <h2 class="section-title">Hi, {{ auth()->user()->name }}</h2>
+                <p class="section-lead">
+                    Change information about yourself on this page.
+                </p>
+                <div class="row mt-sm-4">
+                    <div class="col-12 col-md-12 col-lg-5">
+                        <div class="card profile-widget">
+                            <div class="profile-widget-header">
+                                <img alt="image" src="{{ asset('img/avatar/avatar-1.png') }}"
+                                    class="rounded-circle profile-widget-picture">
+                                <div class="profile-widget-items">
+                                    <div class="profile-widget-item">
+                                        <div class="profile-widget-item-label">Email</div>
+                                        <div class="profile-widget-item-value">{{ auth()->user()->email }}</div>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="profile-widget-description">
+                                <div class="profile-widget-name">{{ auth()->user()->name }} <div
+                                        class="text-muted d-inline font-weight-normal">
+                                        <div class="slash"></div> {{ auth()->user()->role }}
+                                    </div>
+                                </div>
+                                {{ auth()->user()->name }} - {{ auth()->user()->unique_number }} merupakan seorang
+                                {{ auth()->user()->role }} di program studi {{ auth()->user()->department }}.
+                            </div>
+                            <div class="card-footer text-center">
+                                <a href="{{ route('users.edit', $user->id) }}" class="btn btn-primary">Edit
+                                    Profile</a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+    </div>
+@endsection
+
+@push('scripts')
+    <!-- JS Libraies -->
+    <script src="{{ asset('library/summernote/dist/summernote-bs4.js') }}"></script>
+
+    <!-- Page Specific JS File -->
+@endpush
