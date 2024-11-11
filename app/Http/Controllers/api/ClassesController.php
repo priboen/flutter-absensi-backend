cfc19bbdcad11c528848c3c2e7fa6364d8a057fb
Adri Khamid <muhamad.adri.ft21@mail.umy.ac.id>
2024-11-12 00:01:54 +0700

    mapping API data

--- app/Http/Controllers/api/ClassesController.php
+++ app/Http/Controllers/api/ClassesController.php
@@ -20,8 +20,27 @@ class ClassesController extends Controller
         $courses = Classes::with(['course.classroom'])
             ->where('user_id', $user->id)
             ->get();
+
+        $dataMK = $courses->map(function ($course) {
+            $timeIn = \Carbon\Carbon::parse($course->course->time_in);
+            $totalMinutes = $course->course->credits * 50;
+            $timeOut = $timeIn->copy()->addMinutes($totalMinutes);
+            return [
+                'id' => $course->id,
+                'course' => $course->course->name,
+                'course_code' => $course->course->courses_code,
+                'credits' => $course->course->credits,
+                'classroom' => $course->course->classroom->name,
+                'building_name' => $course->course->classroom->building_name,
+                'time_in' => $timeIn->format('H:i'),
+                'time_out' => $timeOut->format('H:i'),
+                'latitude' => $course->course->classroom->latitude,
+                'longitude' => $course->course->classroom->longitude,
+            ];
+        });
         return response()->json([
-            'data' => $courses
+            'message' => 'success',
+            'data' => $dataMK
         ], 200);
     }
 }
