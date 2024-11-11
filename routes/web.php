575808cebe976c03a8cee58749b64dae6723f78a
Adri Khamid <muhamad.adri.ft21@mail.umy.ac.id>
2024-11-11 22:31:23 +0700

    add route for Group Class

--- routes/web.php
+++ routes/web.php
@@ -4,6 +4,7 @@ use App\Http\Controllers\AttendanceController;
 use App\Http\Controllers\ClassesController;
 use App\Http\Controllers\ClassroomController;
 use App\Http\Controllers\CourseController;
+use App\Http\Controllers\GroupClassController;
 use App\Http\Controllers\PermissionController;
 use App\Http\Controllers\UserController;
 use App\Http\Middleware\RoleMiddleware;
@@ -18,12 +19,13 @@ Route::middleware(['auth'])->group(function () {
         return view('pages.dashboard', ['type_menu' => 'home']);
     })->name('home');
 
-    Route::middleware([RoleMiddleware::class . 'admin'])->group(function () {
-        Route::resource('users', UserController::class);
-        Route::resource('courses', CourseController::class);
-        Route::resource('classrooms', ClassroomController::class);
-        Route::resource('classes', ClassesController::class);
-        Route::resource('attendances', AttendanceController::class);
-        Route::resource('permissions', PermissionController::class);
-    });
+    Route::resource('users', UserController::class);
+    Route::resource('courses', CourseController::class);
+    Route::resource('classrooms', ClassroomController::class);
+    Route::resource('classes', ClassesController::class);
+    Route::resource('attendances', AttendanceController::class);
+    Route::resource('permissions', PermissionController::class);
+    Route::resource('groups', GroupClassController::class);
+
+    // Route::middleware([RoleMiddleware::class . 'admin'])->group(function () {});
 });
