1c8696fc0c30fa5950f7ffb93eccdfa8811c5bad
Adri Khamid <muhamad.adri.ft21@mail.umy.ac.id>
2024-11-21 10:12:50 +0700

    add try and catch validation

--- app/Http/Controllers/api/NoteController.php
+++ app/Http/Controllers/api/NoteController.php
@@ -10,64 +10,98 @@ class NoteController extends Controller
 {
     public function index(Request $request)
     {
-        $notes = Note::where('user_id', $request->user()->id)->orderBy('id', 'desc')->get();
-        return response()->json([
-            'data' => $notes
-        ], 200);
+        try {
+            $notes = Note::where('user_id', $request->user()->id)->orderBy('id', 'desc')->get();
+            return response()->json([
+                'data' => $notes
+            ], 200);
+        } catch (\Exception $e) {
+            return response()->json([
+                'message' => 'Terjadi kesalahan server',
+                'error' => $e->getMessage()
+            ], 500);
+        }
     }
 
     public function store(Request $request)
     {
-        $request->validate([
-            'title' => 'required',
-            'note' => 'required',
-        ]);
+        try {
+            $request->validate([
+                'title' => 'required',
+                'note' => 'required',
+            ]);
 
-        $note = new Note();
-        $note->user_id = $request->user()->id;
-        $note->title = $request->title;
-        $note->note = $request->note;
-        $note->save();
+            $note = new Note();
+            $note->user_id = $request->user()->id;
+            $note->title = $request->title;
+            $note->note = $request->note;
+            $note->save();
 
-        return response()->json([
-            'status' => 'Catatan berhasil disimpan!',
-            'data' => $note
-        ], 200);
+            return response()->json([
+                'status' => 'Catatan berhasil disimpan!',
+                'data' => $note
+            ], 200);
+        } catch (\Exception $e) {
+            return response()->json([
+                'message' => 'Terjadi kesalahan server',
+                'error' => $e->getMessage()
+            ], 500);
+        }
     }
 
     public function show($id)
     {
-        $note = Note::find($id);
-        return response()->json([
-            'data' => $note
-        ], 200);
+        try {
+            $note = Note::find($id);
+            return response()->json([
+                'data' => $note
+            ], 200);
+        } catch (\Exception $e) {
+            return response()->json([
+                'message' => 'Terjadi kesalahan server',
+                'error' => $e->getMessage()
+            ], 500);
+        }
     }
 
     public function update(Request $request, $id)
     {
-        $request->validate([
-            'title' => 'required',
-            'note' => 'required',
-        ]);
+        try {
+            $request->validate([
+                'title' => 'required',
+                'note' => 'required',
+            ]);
 
-        $note = Note::find($id);
-        $note->title = $request->title;
-        $note->note = $request->note;
-        $note->save();
+            $note = Note::find($id);
+            $note->title = $request->title;
+            $note->note = $request->note;
+            $note->save();
 
-        return response()->json([
-            'status' => 'Catatan berhasil diupdate!',
-            'data' => $note
-        ], 200);
+            return response()->json([
+                'status' => 'Catatan berhasil diupdate!',
+                'data' => $note
+            ], 200);
+        } catch (\Exception $e) {
+            return response()->json([
+                'message' => 'Terjadi kesalahan server',
+                'error' => $e->getMessage()
+            ], 500);
+        }
     }
 
     public function destroy($id)
     {
-        $note = Note::find($id);
-        $note->delete();
-
-        return response()->json([
-            'status' => 'Catatan berhasil dihapus!',
-        ], 200);
+        try {
+            $note = Note::find($id);
+            $note->delete();
+            return response()->json([
+                'status' => 'Catatan berhasil dihapus!',
+            ], 200);
+        } catch (\Exception $e) {
+            return response()->json([
+                'message' => 'Terjadi kesalahan server',
+                'error' => $e->getMessage()
+            ], 500);
+        }
     }
 }
