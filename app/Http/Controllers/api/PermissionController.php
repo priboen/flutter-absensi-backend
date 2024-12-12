cf2443cee46d8bab437beefd99cef1c91d3ccf27
Adri Khamid <muhamad.adri.ft21@mail.umy.ac.id>
2024-12-12 13:26:35 +0700

    fixed image bug

--- app/Http/Controllers/api/PermissionController.php
+++ app/Http/Controllers/api/PermissionController.php
@@ -10,29 +10,68 @@ class PermissionController extends Controller
 {
     public function store(Request $request)
     {
-        $request->validate([
-            'class_id' => 'required',
-            'date_permission' => 'required',
-            'reason' => 'required',
-        ]);
-        $permission = new Permission();
-        $permission->class_id = $request->class_id;
-        $permission->date_permission = $request->date_permission;
-        $permission->reason = $request->reason;
-        $permission->image = $request->image;
-        // $permission->is_approved = 0;
-        $permission->is_approved = $request->has('is_approved') ? $request->is_approved : null;
+        try {
+            $request->validate([
+                'class_id' => 'required',
+                'date_permission' => 'required',
+                'reason' => 'required',
+            ]);
+            $permission = new Permission();
+            $permission->class_id = $request->class_id;
+            $permission->date_permission = $request->date_permission;
+            $permission->reason = $request->reason;
+            $permission->image = $request->image;
+            $permission->is_approved = $request->has('is_approved') ? $request->is_approved : null;
 
-        if ($request->hasFile('image')) {
-            $request->file('image')->move('images/permission/', $request->file('image')->getClientOriginalName());
-            $permission->image = $request->file('image')->getClientOriginalName();
+            if ($request->hasFile('image')) {
+                $request->file('image')->move('images/permission/', $request->file('image')->getClientOriginalName());
+                $permission->image = $request->file('image')->getClientOriginalName();
+                $permission->save();
+            }
             $permission->save();
+
+            return response()->json([
+                'status' => 'success',
+                'data' => $permission
+            ], 200);
+        } catch (\Exception $e) {
+            return response()->json([
+                'status' => 'error',
+                'message' => $e->getMessage()
+            ], 500);
         }
-        $permission->save();
+    }
+
+    public function index(Request $request)
+    {
+
+        try{
+            $currentUser = $request->user();
+        $query = Permission::with(['class.course'])->whereHas('class', function ($q) use ($currentUser) {
+            $q->where('user_id', $currentUser->id);
+        });
+
+        $permissions = $query->get();
+
+        $permissionData = $permissions->map(function ($item) {
+            return [
+                'id' => $item->id,
+                'course_name' => $item->class->course->name ?? null,
+                'date_permission' => $item->date_permission,
+                'reason' => $item->reason,
+                'is_approved' => $item->is_approved,
+            ];
+        });
 
         return response()->json([
             'status' => 'success',
-            'data' => $permission
+            'data' => $permissionData
         ], 200);
+        }catch(\Exception $e){
+            return response()->json([
+                'status' => 'Terjadi kesalahan server',
+                'message' => $e->getMessage()
+            ], 500);
+        }
     }
 }
